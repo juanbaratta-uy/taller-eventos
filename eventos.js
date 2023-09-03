188a7ce2bec419b7fd9244327fe10ed76d211b8d
Juan Baratta <juanpablobaratta@gmail.com>
2023-09-03 19:07:55 -0300

    Termine

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+document.getElementById("div").addEventListener('click', function(){
+    alert("Hola! Soy el div")
+    console.log('hola');
+ });
+});
